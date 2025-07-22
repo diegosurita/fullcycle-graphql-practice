4dc7e8e3b13450a6fb714c88b01167c9e8a30e36
Diego Surita <diego.surita@webpt.com>
2025-07-22 08:43:39 -0300

    implement create course resolver

--- graph/resolver.go
+++ graph/resolver.go
@@ -8,4 +8,5 @@ import "github.com/diegosurita/fullcycle-graphql-practice/internal/database"
 
 type Resolver struct {
 	CategoryDB *database.Category
+	CourseDB   *database.Course
 }
---
+++ internal/database/course.go
@@ -0,0 +1,62 @@
+package database
+
+import (
+	"database/sql"
+
+	"github.com/google/uuid"
+)
+
+func NewCourse(db *sql.DB) *Course {
+	return &Course{
+		db: db,
+	}
+}
+
+type Course struct {
+	db          *sql.DB
+	ID          string
+	Title       string
+	Description *string
+	CategoryID  *string
+}
+
+func (c *Course) Create(title string, description string, categoryID string) (*Course, error) {
+	id := uuid.New().String()
+	query := "INSERT INTO courses (id, title, description, category_id) VALUES (?, ?, ?, ?)"
+	_, err := c.db.Exec(query, id, title, description, categoryID)
+	if err != nil {
+		return nil, err
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &Course{
+		ID:          id,
+		Title:       title,
+		Description: &description,
+		CategoryID:  &categoryID,
+	}, nil
+}
+
+func (c *Course) FindAll() ([]*Course, error) {
+	rows, err := c.db.Query("SELECT id, title, description, category_id FROM courses")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var courses []*Course
+	for rows.Next() {
+		var course Course
+		if err := rows.Scan(&course.ID, &course.Title, &course.Description, &course.CategoryID); err != nil {
+			return nil, err
+		}
+		courses = append(courses, &course)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return courses, nil
+}
